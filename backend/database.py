9c179d6b00c8215bb5cc75f74562500407786273
Raghav Ramesh <raghavramesh@mac.3500.dhcp.asu.edu>
2025-09-20 13:12:40 -0700

    hi

---
+++ backend/database.py
@@ -0,0 +1,16 @@
+# Import the create_engine function to connect SQLAlchemy to a database
+from sqlalchemy import create_engine
+
+# Import declarative_base to create a base class for our database models (tables)
+from sqlalchemy.ext.declarative import declarative_base
+
+# Import sessionmaker to create database sessions (connections to run queries)
+from sqlalchemy.orm import sessionmaker
+
+# Uses SQLite database and saves the file in the current folder
+SQLALCHEMY_DATABASE_URL = "sqlite:///./medshare.db"
+
+# Engine manages the connection to the database
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+
+# wow
